f8ce35162c84dc5ef07e2e01970417b8bf1900e9
victor-at-dev <viktornjoroge@gmail.com>
2026-05-28 15:39:17 +0300

    added some texts

--- src/pages/finance/Finance.jsx
+++ src/pages/finance/Finance.jsx
@@ -1,7 +1,9 @@
 export default function Finance(){
     return(
         <div>
-            <p>Current prices include :....</p>
+            <p>Current prices include :....
+                welcome to our repo ,till we meet again bro
+            </p>
         </div>
     )
 }
